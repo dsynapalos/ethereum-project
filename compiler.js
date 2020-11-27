7767577ddd133056f1f9034afa6ba2ec5ba65894
Dimitrios Synapalos <dim.synapalos@gmail.com>
2020-11-27 13:08:31 +0200

    1

--- compiler.js
+++ compiler.js
@@ -2,8 +2,8 @@ const path = require(path);
 const fs = require(fs);
 const solc = require(solc);
 
-const pathtocontract = path.resolve(__dirname,'contract.sol')
-const contract = fs.readFileSync(pathtocontract,'utf8')
+const pathtocontract = path.resolve(__dirname,'contract.sol');
+const contract = fs.readFileSync(pathtocontract,'utf8');
 
-var compiled = solc.compile(contract, 1)
-console.log(compiled)
+var compiled = solc.compile(contract, 1);
+console.log(compiled);
