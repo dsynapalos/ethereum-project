5a36ea2aa2e4645f2e0db59297c23bdad815342a
Dimitrios Synapalos <dim.synapalos@gmail.com>
2020-11-27 15:15:27 +0200

    3

--- compiler.js
+++ compiler.js
@@ -5,5 +5,5 @@ const solc = require('solc');
 const pathtocontract = path.resolve(__dirname,'contract.sol');
 const contract = fs.readFileSync(pathtocontract,'utf8');
 
-var compiled = solc.compile(contract, 1);
-console.log(compiled);
+// console.log(solc.compile(contract, 1));
+module.exports = solc.compile(contract, 1).contracts[':simpleSwitch'];
